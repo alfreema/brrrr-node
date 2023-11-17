664752c485e98fb93d52092c4219a1cbea7cbd83
Aaron Freeman <aaron@sendthisfile.com>
2023-11-17 07:56:40 -0600

    Remove unused function from cash-purchase

--- simulations/buy/cash-purchase.js
+++ simulations/buy/cash-purchase.js
@@ -1,13 +1,3 @@
-// Function to calculate monthly mortgage payment
-function calculateMonthlyPayment(loanAmount, annualInterestRate, loanTermYears) {
-  const monthlyInterestRate = annualInterestRate / 12 / 100;
-  const totalPayments = loanTermYears * 12;
-  const discountFactor = (Math.pow(1 + monthlyInterestRate, totalPayments) - 1) /
-    (monthlyInterestRate * Math.pow(1 + monthlyInterestRate, totalPayments));
-  const monthlyPayment = loanAmount / discountFactor;
-  return monthlyPayment;
-}
-
 // Function to simulate property purchase
 const simulate = ({ propertyPrice, closingCostRate }) => {
   const closingCosts = propertyPrice * (closingCostRate / 100);
