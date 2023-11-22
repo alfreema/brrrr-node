75f4c1132155d9572acc8fba56310f23d930acda
Aaron Freeman <aaron@sendthisfile.com>
2023-11-22 11:31:20 -0600

    Add path-browserify polyfill dependency

--- simulations/buy.js
+++ simulations/buy.js
@@ -1,59 +1,32 @@
-const joinPath = (...paths) => {
-  // Check if the process object exists (Node.js environment)
-  if (typeof process !== 'undefined' && process?.cwd) {
-    const path = require('path');
-    return path.join(...paths);
-  } else {
-    // Browser environment
-    if (typeof require !== 'undefined') {
-      try {
-        // Attempt to use 'path' module
-        const path = require('path');
-        return path.join(...paths);
-      } catch (error) {
-        // If 'path' module is not available, use '/'
-        return paths.join('/');
-      }
-    } else {
-      // If 'require' is not defined, use '/'
-      return paths.join('/');
-    }
-  }
-};
+//const path = require('path');
+const path = typeof window === 'undefined' ? require('path-browserify') : require('path');
+
 
 const purchase = (lib, terms, propertyOwnershipRates) => {
-  const buyLib = require(lib);
-  const purchaseResult = buyLib.simulate(terms);
-
-  // Use joinPath for path resolution
-  const carryCostsLibPath = joinPath(__dirname, 'buy/carrying-costs.js');
-  const carryCostsLib = require(carryCostsLibPath);
-  
-  purchaseResult.carryCosts = carryCostsLib.simulate({
+  const buyLib = require(lib)
+  const purchase = buyLib.simulate(terms)
+  const carryCostsLib = require(path.join(__dirname, 'buy/carrying-costs.js'))
+  purchase.carryCosts = carryCostsLib.simulate({
     propertyPrice: terms.propertyPrice,
-    monthlyLoanPayment: purchaseResult.monthlyPayment,
+    monthlyLoanPayment: purchase.monthlyPayment,
     ...propertyOwnershipRates
-  });
-
-  return purchaseResult;
-};
+  })
+  return purchase
+}
 
 const simulate = financeDetails => {
-  const getFilePath = subPath => joinPath(__dirname, `buy/${subPath}.js`);
-
-  if (financeDetails?.traditionalMortgageLoan) {
-    return purchase(getFilePath('traditional-mortgage-loan'), financeDetails.traditionalMortgageLoan, financeDetails.propertyOwnershipRates);
-  } else if (financeDetails?.cashPurchase) {
-    return purchase(getFilePath('cash-purchase'), financeDetails.cashPurchase, financeDetails.propertyOwnershipRates);
-  } else if (financeDetails?.hardMoneyLoan) {
-    return purchase(getFilePath('hard-money-loan'), financeDetails.hardMoneyLoan, financeDetails.propertyOwnershipRates);
+  if(financeDetails?.traditionalMortgageLoan) {
+    return purchase(path.join(__dirname, 'buy/traditional-mortgage-loan.js'), financeDetails.traditionalMortgageLoan, financeDetails.propertyOwnershipRates)
+  } else if(financeDetails?.cashPurchase) {
+    return purchase(path.join(__dirname, 'buy/cash-purchase.js'), financeDetails.cashPurchase, financeDetails.propertyOwnershipRates)
+  } else if(financeDetails?.hardMoneyLoan) {
+    return purchase(path.join(__dirname, 'buy/hard-money-loan.js'), financeDetails.hardMoneyLoan, financeDetails.propertyOwnershipRates)
   }
-
-  console.warn('No finance method was recognized.');
-  console.warn('Try passing a "traditionalMortgageLoan" property.');
-  return null;
-};
+  console.warn('No finance method was recognized.')
+  console.warn('Try passing a "traditionalMortgageLoan" property.')
+  return null
+}
 
 module.exports = {
   simulate
-};
+}
