1805ab192526a7c23080a4dc20af83e2a90a6eaa
Aaron Freeman <aaron@sendthisfile.com>
2023-11-28 07:52:12 -0600

    Revert index.js __dirname pathing

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-const path = require('path');
-
-const buy = require(path.join(__dirname, 'simulations/buy'));
-const rehab = require(path.join(__dirname, 'simulations/rehab'));
-const rent = require(path.join(__dirname, 'simulations/rent'));
-const refinance = require(path.join(__dirname, 'simulations/refinance'));
-const percentage = require(path.join(__dirname, 'math/percentage'));
+const buy = require('./simulations/buy');
+const rehab = require('./simulations/rehab');
+const rent = require('./simulations/rent');
+const refinance = require('./simulations/refinance');
+const percentage = require('./math/percentage');
 
 module.exports = {
   buy,
