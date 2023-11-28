a6e3a5825937db661596fdaa6b7eebf9c5ed8828
Aaron Freeman <aaron@sendthisfile.com>
2023-11-28 08:10:34 -0600

    Refactor refinance by removing dependency on an expression

--- simulations/refinance.js
+++ simulations/refinance.js
@@ -1,13 +1,15 @@
+const traditionalMortgageRefinanceLib = require('./refinance/traditional-mortgage-refinance.js')
+const commercialLoanLib = require('./refinance/commercial-loan.js')
+
 const refinance = (lib, terms, investment) => {
-  const refinanceLib = require(lib)
-  return refinanceLib.simulate(terms, investment)
+  return lib.simulate(terms, investment)
 }
 
 const simulate = financeDetails => {
   if(financeDetails?.traditionalMortgageRefinance) {
-    return refinance('./refinance/traditional-mortgage-refinance.js', financeDetails.traditionalMortgageRefinance, financeDetails.investment)
+    return refinance(traditionalMortgageRefinanceLib, financeDetails.traditionalMortgageRefinance, financeDetails.investment)
   } else if(financeDetails?.commercialLoan) {
-   return refinance('./refinance/commercial-loan.js', financeDetails.commercialLoan, financeDetails.investment)
+   return refinance(commercialLoanLib, financeDetails.commercialLoan, financeDetails.investment)
   }
   console.warn('No refinance method was recognized.')
   console.warn('Try passing a "traditionalMortgageRefinance" property.')
