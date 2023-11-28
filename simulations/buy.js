95b31e697ae5631ea195997902b09bfd9256bde6
Aaron Freeman <aaron@sendthisfile.com>
2023-11-28 08:04:28 -0600

    Refactor buy.js to not be dynamic when requiring a library

--- simulations/buy.js
+++ simulations/buy.js
@@ -1,9 +1,10 @@
-const path = require('path');
+const traditionalMortgageLoanLib = require('./buy/traditional-mortgage-loan.js')
+const cashPurchaseLib = require('./buy/cash-purchase.js')
+const hardMoneyLoanLib = require('./buy/hard-money-loan.js')
 
 const purchase = (lib, terms, propertyOwnershipRates) => {
-  const buyLib = require(lib)
-  const purchase = buyLib.simulate(terms)
-  const carryCostsLib = require(path.join(__dirname, 'buy/carrying-costs.js'))
+  const purchase = lib.simulate(terms)
+  const carryCostsLib = require('./buy/carrying-costs.js')
   purchase.carryCosts = carryCostsLib.simulate({
     propertyPrice: terms.propertyPrice,
     monthlyLoanPayment: purchase.monthlyPayment,
@@ -13,13 +14,12 @@ const purchase = (lib, terms, propertyOwnershipRates) => {
 }
 
 const simulate = financeDetails => {
-  console.log('__dirname=', __dirname)
   if(financeDetails?.traditionalMortgageLoan) {
-    return purchase(path.join(__dirname, 'buy/traditional-mortgage-loan.js'), financeDetails.traditionalMortgageLoan, financeDetails.propertyOwnershipRates)
+    return purchase(traditionalMortgageLoanLib, financeDetails.traditionalMortgageLoan, financeDetails.propertyOwnershipRates)
   } else if(financeDetails?.cashPurchase) {
-    return purchase(path.join(__dirname, 'buy/cash-purchase.js'), financeDetails.cashPurchase, financeDetails.propertyOwnershipRates)
+    return purchase(cashPurchaseLib, financeDetails.cashPurchase, financeDetails.propertyOwnershipRates)
   } else if(financeDetails?.hardMoneyLoan) {
-    return purchase(path.join(__dirname, 'buy/hard-money-loan.js'), financeDetails.hardMoneyLoan, financeDetails.propertyOwnershipRates)
+   return purchase(hardMoneyLoanLib, financeDetails.hardMoneyLoan, financeDetails.propertyOwnershipRates)
   }
   console.warn('No finance method was recognized.')
   console.warn('Try passing a "traditionalMortgageLoan" property.')
